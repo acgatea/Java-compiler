dfb865f5b639fa99385eaafc583c9144e286f4f9
Alex Gatea <acgatea@users.noreply.github.com>
2021-10-16 18:56:49 -0400

    Delete runtime_exception directory

--- runtime_exception/Je_InvalidArrayAccess.java
+++
@@ -1,9 +0,0 @@
-public class Je_InvalidArrayAccess{
-	public Je_InvalidArrayAccess(){ }
-
-	public static int test(){
-		int[] myArr=new int[3];
-		myArr[3]=123;
-		return myArr[3];
-	}
-}
--- runtime_exception/Je_InvalidDivision.java
+++
@@ -1,9 +0,0 @@
-public class Je_InvalidDivision{
-	public Je_InvalidDivision(){ }
-
-	public static int test(){
-		int[] a=new int[3];
-		int b = 5/ a[0];
-		return 123;	
-	}
-}
